5e421126c4c448a27316f5a5fa550fcb68c29473
aawadall <aawadall@ualberta.ca>
2018-03-17 18:34:14 -0400

    tested http.HandleFunc and http.Handle

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
+type myHandler struct {
+	greeting string
+}
+
+func (mh myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(fmt.Sprintf("%v world", mh.greeting)))
+}
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello Go!"))
+		w.Write([]byte("Hello World: from HandleFunc"))
 	})
-	http.ListenAndServe(":1000", nil)
+	//http.Handle("/", &myHandler{greeting: "Hello"})
+	http.ListenAndServe(":8888", nil)
 }
